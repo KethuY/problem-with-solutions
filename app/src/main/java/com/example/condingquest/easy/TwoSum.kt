74ac92f7e35b5feb1a764244cf83cc72ac0e9ad1
Kethu <kethu.yaramma@uobgroup.com>
2022-12-13 22:59:08 +0530

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    You may assume that each input would have exactly one solution, and younmay not use the same element twice.
    
    You can return the answer in any order

---
+++ app/src/main/java/com/example/condingquest/easy/TwoSum.kt
@@ -0,0 +1,35 @@
+package com.example.condingquest.easy
+
+fun main() {
+    val numberArray = intArrayOf(2, 7, 11, 15,5,4)
+    val target = 9
+    val resultArray = twoSum(numberArray, target)
+    resultArray.let {
+       for (element in resultArray){
+           print(element)
+           print(" ")
+       }
+   }
+}
+
+fun twoSum(nums: IntArray, target: Int): List<Int> {
+    val indexes = arrayListOf<Int>()
+    for (i in nums.indices) {
+        for (j in i + 1 until nums.size) {
+           /* Solution1
+            if (nums[j] == target - nums[i]) {
+                indexes.add(i)
+                indexes.add(j)
+            }*/
+
+            // solution 2
+            if (target == nums[j] + nums[i]) {
+                indexes.add(i)
+                indexes.add(j)
+            }
+        }
+    }
+    // In case there is no solution, we'll just return null
+    // In case there is no solution, we'll just return null
+    return indexes
+}
